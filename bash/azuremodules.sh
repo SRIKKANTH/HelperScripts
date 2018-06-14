533c0355ef6d84592dca9001e2d08305af4ee469
Srikanth Myakam <m.srikanth.m@gmail.com>
2018-06-14 16:06:50 -0700

    Added docker realted routines

--- bash/azuremodules.sh
+++ bash/azuremodules.sh
@@ -414,7 +414,7 @@ function remove_cmd_from_startup ()
 	fi
 }
 
-install_azcopy ()
+function install_azcopy ()
 {
 
   if [[ `which yum` != "" ]]; then
@@ -440,7 +440,7 @@ install_azcopy ()
   check_exit_status "Installation of azcopy"
 }
 
-upload_files_to_blob_storage ()
+function upload_files_to_blob_storage ()
 {
 	filename=$1
 	blob_storage_url=$2
@@ -504,3 +504,123 @@ function set_service_status ()
     fi
 	echo "Currenr status of service $ServiceName:" `GetServiceStatus $ServiceName`
 }
+
+function InstallDockerFromGetDockerDotCom ()
+{
+    sudo docker --version | grep "Docker version" | LogMsg
+    if [ $? -eq 0 ]
+	then
+		sudo systemctl start docker
+		echo "Docker already installed" | LogMsg
+    else
+		echo "Installing Docker" | LogMsg
+		#TODO Should we install docker via someother means ?for example apt-get install docker-ce
+		wget -qO- https://get.docker.com/ | sh
+
+		if [ $? -ne 0 ] 
+		then
+			echo "docker install failed" | LogMsg
+			return 1
+		fi
+		
+		echo "Installing Docker:Done" | LogMsg
+		# Commenting below out as currently custom extension script is run as root. 
+		# The virtualmachine admin user has sudo permission currently so below may not be required anyways.
+		# if required put in a seperate function and get admin user from FSM as here the $USER will always be root
+		#sudo usermod -aG docker $USER
+    fi
+    DockerStatus=`get_service_status docker`
+	if [ "x$DockerStatus" == "running" ]
+	then
+		echo "Docker Installed Succesfully"  | LogMsg
+		return 1
+	elif [ "x$DockerStatus" == "dead" ]
+	then
+		echo "Docker Installed succesfully but service not started" | LogMsg
+		return 1
+	elif [ "x$DockerStatus" == "ServiceNotFound" ]
+	then
+		echo "Error: Docker Installation Failed" | LogMsg
+		return 2
+	fi
+}
+
+function InstallDockerFromRepo ()
+{
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - | LogMsg  
+	check_exit_status | LogMsg 
+	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | LogMsg
+	check_exit_status | LogMsg
+	sudo apt-get update | LogMsg
+	check_exit_status  | LogMsg
+	sudo apt-cache policy docker-ce | LogMsg
+	check_exit_status  | LogMsg
+	sudo apt-get install -y docker-ce | LogMsg
+	check_exit_status  | LogMsg
+	sudo systemctl status docker | LogMsg
+	check_exit_status  | LogMsg
+	DockerStatus=`get_service_status docker`
+	if [ "x$DockerStatus" == "running" ]
+	then
+		echo "Docker Installed Succesfully"  | LogMsg
+		return 1
+	elif [ "x$DockerStatus" == "dead" ]
+	then
+		echo "Docker Installed succesfully but service not started" | LogMsg
+		return 1
+	elif [ "x$DockerStatus" == "ServiceNotFound" ]
+	then
+		echo "Error: Docker Installation Failed" | LogMsg
+		return 2
+	fi
+}
+
+function InstallDocker()
+{
+    if [[ `which docker` != "" ]]
+    then
+        echo "Info: 'docker' is already installed skipping ..."
+    else
+		echo "Info: Installing 'docker' ..."
+
+		InstallDockerFromRepo
+		Status=$?
+		if [ $Status -eq 1 ]
+		then
+			set_service_status docker restart
+		elif [ $Status -gt 1 ]
+		then
+			echo "Warning: InstallDockerFromRepo failed to install 'docker'" | LogMsg
+			InstallDockerFromGetDockerDotCom
+			Status=$?
+			if [ $Status -eq 1 ]
+			then
+				set_service_status docker restart
+			elif [ $Status -gt 1 ]
+			then
+				echo "Error: Failed to Install docker exitting now" | LogMsg
+				echo "DOCKER_INSTALLATION_FAILED" | UpdateStatus
+			fi
+		fi
+		
+		echo "Info: Installation of docker succesfully finished" | LogMsg		
+		echo "DOCKER_INSTALLATION_SUCCESS" | UpdateStatus
+
+		if [ $? -ne 0 ] 
+		then 
+			exit 1 
+		fi
+	fi
+}
+
+function stop_and_remove_all_containers()
+(
+    docker stop $(docker ps -a -q)
+    docker rm $(docker ps -a -q)
+)
+
+function remove_all_images()
+(
+    docker rmi $(docker images -a -q)
+)
+
